2019983ab41e580535c851fd662e5bc0ccee1c68
blueream74 <bluedream7004@gmail.com>
2023-09-04 20:27:54 +0900

    Update DB connection config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,8 +113,12 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory',
+        'USER': 'inventory',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
